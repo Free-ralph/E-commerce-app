7e5310c8308230a5a073fd83e6fd9522934404c9
Free-Ralph <okosachideragia@gmail.com>
2023-09-15 10:25:32 +0100

    added manifest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: { manifest: true },
+  base: process.env.VITE_mode === "production" ? "/static/" : "/",
   plugins: [react()],
-})
+});
